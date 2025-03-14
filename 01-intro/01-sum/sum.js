1a381e35952b0ec75bf4cb9e62d4e1fc2a72acec
dmitry.koryanov <dmitry.koryanov@yandex.ru>
2025-03-14 11:15:33 +0100

    Задача "Сумма двух чисел"

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,3 +1,8 @@
 export default function sum(a, b) {
   /* ваш код */
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new TypeError("a and b should be numbers");
+  }
+
+  return a + b;
 }
