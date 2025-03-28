43e893dcb63e719b2481056f47e12af1b484fd95
dmitry.koryanov <dmitry.koryanov@yandex.ru>
2025-03-28 14:04:07 +0100

    Задание: Использование Middleware, Pipes, Guards, Interceptors и Error Handlers

--- 04-request-lifecycle/01-nestjs-components/guards/roles.guard.ts
+++ 04-request-lifecycle/01-nestjs-components/guards/roles.guard.ts
@@ -1,5 +1,22 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
 
+@Injectable()
 export class RolesGuard implements CanActivate {
-  canActivate(context: ExecutionContext) {}
+  canActivate(context: ExecutionContext) {
+    const ctx = context.switchToHttp();
+    const req = ctx.getRequest();
+
+    if (req.headers["x-role"] !== "admin") {
+      throw new ForbiddenException("Доступ запрещён: требуется роль admin");
+    }
+
+    return true;
+    // return req.headers["x-role"] === "admin";
+  }
 }
--- 04-request-lifecycle/01-nestjs-components/tasks/tasks.controller.ts
+++ 04-request-lifecycle/01-nestjs-components/tasks/tasks.controller.ts
@@ -7,10 +7,15 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  UseGuards,
+  UseInterceptors,
 } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { CreateTaskDto, UpdateTaskDto } from "./task.model";
+import { RolesGuard } from "../guards/roles.guard";
+import { ApiVersionInterceptor } from "../interceptors/api-version.interceptor";
 
+@UseInterceptors(ApiVersionInterceptor)
 @Controller("tasks")
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
@@ -20,11 +25,17 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get("error")
+  throwError() {
+    throw new Error("Mock error for testing");
+  }
+
   @Get(":id")
   getTaskById(@Param("id", ParseIntPipe) id: number) {
     return this.tasksService.getTaskById(id);
   }
 
+  @UseGuards(RolesGuard)
   @Post()
   createTask(@Body() task: CreateTaskDto) {
     return this.tasksService.createTask(task);
@@ -38,6 +49,7 @@ export class TasksController {
     return this.tasksService.updateTask(id, task);
   }
 
+  @UseGuards(RolesGuard)
   @Delete(":id")
   deleteTask(@Param("id", ParseIntPipe) id: number) {
     return this.tasksService.deleteTask(id);
